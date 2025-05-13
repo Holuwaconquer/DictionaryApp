20967f80fed100022b285e0101d85d563f11a022
Okikiola Fakorede <holuwaconquer488@gmail.com>
2025-05-13 22:35:33 +0100

    added PWA option

--- dictionary.js
+++ dictionary.js
@@ -83,3 +83,36 @@ let audioUrl;
             });
         }
     });
+
+
+    if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+        }, function(err) {
+            console.log('Service Worker registration failed:', err);
+        });
+    });
+}
+
+// Handle Install Prompt
+let deferredPrompt;
+let installBtn = document.getElementById('installBtn')
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    installBtn.style.display = 'block';
+
+    installBtn.addEventListener('click', () => {
+        installBtn.style.display = 'none';
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the install prompt');
+            } else {
+                console.log('User dismissed the install prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
